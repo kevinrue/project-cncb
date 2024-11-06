776024eae4f407192f626aeaa725ebaa41a6e07c
Kevin Rue-Albrecht <kevinrue@users.noreply.github.com>
2024-11-06 15:07:01 +0000

    add sam to bam script

--- genome_bwa_mem.sh
+++ genome_bwa_mem.sh
@@ -5,7 +5,7 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=12
 #SBATCH --mem=10G
-#SBATCH --partition=long
+#SBATCH --partition=short
 
 # Environment:
 #SBATCH --export=NONE
---
+++ genome_sam_to_bam.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Resources:
+#SBATCH --time=0-00:15:00  # DAYS-HOURS:MINUTES:SECONDS
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=12
+#SBATCH --mem=10G
+#SBATCH --partition=short
+
+# Environment:
+#SBATCH --export=NONE
+
+# What to run:
+module load samtools/1.17
+
+cd /ceph/project/cncb/albrecht
+
+samtools view -@ 12 -bo genome_sequencing/aln-pe.bam genome_sequencing/aln-pe.sam
+
